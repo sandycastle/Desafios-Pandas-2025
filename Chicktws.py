4f0f60db0e852d46cf9a07494420db4f314a025d
sandycastle <99694969+sandycastle@users.noreply.github.com>
2025-02-20 12:05:33 -0300

    Update Chicktws.py

--- Chicktws.py
+++ Chicktws.py
@@ -4,15 +4,15 @@ print("Problem statement: An experiment was conducted to measure and compare the
 
 df_Chickwts = data("chickwts")
 
-# Calculate the average chick weight for each feed type
+# 1. Calculate the average chick weight for each feed type
 average_weight = df_Chickwts.groupby('feed')['weight'].mean()
 print("1. Average chick weight for each feed type: \n", average_weight.reset_index())
 
-# Identify the feed that resulted in the highest average weight
+# 2. Identify the feed that resulted in the highest average weight
 highest_avg_feed = average_weight.idxmax()
 print("\n 2. Feed that resulted in the highest average weight:", highest_avg_feed)
 
 
-# Create a table showing the maximum, minimum, and average weight for each feed type
+# 3. Create a table showing the maximum, minimum, and average weight for each feed type
 summary_table = df_Chickwts.groupby('feed')['weight'].agg(['max', 'min', 'mean'])
 print("\n 3. Table showing the maximum, minimum, and average weight for each feed type:\n", summary_table.reset_index())
